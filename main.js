ad5edb5c5e2f7e7ef1b88cddf451bf052d757d82
Rutger Klamer <rutger@rutgerklamer.nl>
2018-03-19 15:43:03 +0100

    Add initial upload

---
+++ main.js
@@ -0,0 +1,74 @@
+let hide = false;
+$(document).ready(function () {
+    $("#time").text(get_time());
+    $("#terminalexe").dblclick(function () {
+        $("#terminal").show("fade", {}, 400);
+        $("#bar").show("fade", {}, 400);
+        $("#tasks").append("<a id='terminaltask' class='left active' style='cursor: default;'> Terminal</a>");
+    });
+
+    $("#bar").draggable();
+    setInterval(function () { $("#time").text(get_time()); }, 1000);
+    $("#red").click(function () {
+        $("#terminal").hide("fade", {}, 400);
+        $("#bar").hide("fade", {}, 400);
+        $('a').remove("#terminaltask");
+    });
+    $("#bar").dblclick(function(){
+        if (!hide) {
+            $("#terminal").hide("fade", {}, 400);
+            $("#terminaltask").removeClass("active");
+        } else {
+            $("#terminal").show("fade", {}, 400);
+            $("#terminaltask").addClass("active");
+        }
+        hide = !hide;
+    });
+
+    $("#green").click(function () {
+        if (!hide) {
+            $("#terminal").hide("fade", {}, 400);
+            $("#bar").hide("fade", {}, 400);
+            $("#terminaltask").removeClass("active");
+        } else {
+            $("#terminal").show("fade", {}, 400);
+            $("#bar").show("fade", {}, 400);
+            $("#terminaltask").addClass("active");
+        }
+        hide = !hide;
+        $("#yellow").click(function () {
+            $("#terminal").show("fade", {}, 400);
+            $("#terminaltask").addClass("active");
+        });
+    });
+});
+ 
+$(document).on('click', '#terminaltask', function () {
+    if (!hide) {
+        $("#terminal").hide("fade", {}, 400);
+        $("#bar").hide("fade", {}, 400);
+        $("#terminaltask").removeClass("active");
+    } else {
+        $("#terminal").show("fade", {}, 400);
+        $("#bar").show("fade", {}, 400);
+        $("#terminaltask").addClass("active");
+    }
+    hide = !hide;
+});
+
+function get_time() {
+    let now = new Date(),
+    ampm = 'am',
+    h = now.getHours();
+    m = now.getMinutes();
+    s = now.getSeconds();
+    if (h >= 12) {
+        if (h > 12)
+            h -= 12;
+        ampm = 'pm';
+    }
+
+    if (m < 10)
+        m = '0' + m;
+    return now.toLocaleDateString() + ' ' + h + ':' + m + ':' + s + ' ' + ampm;
+}
